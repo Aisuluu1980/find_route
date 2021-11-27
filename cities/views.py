ced51861664953ac797e63746b32749572cbeaf5
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-11-27 22:23:54 +0600

    Added HTML form.

--- cities/views.py
+++ cities/views.py
@@ -1,22 +1,30 @@
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from django.views.generic import DetailView
+
 from cities.models import City
 
 __all__ = (
-    'home',
+    'home', 'CityDetailView'
 )
 
 
 
 def home(request, pk=None):
-    if pk:
+    # if pk:
         # city = City.objects.filter(id=pk).first()
         # city = City.objects.get(id=pk)
-        city = get_object_or_404(City, id=pk)
-        context = {'object':city}
-        return render(request, 'cities/detail.html', context)
+        # city = get_object_or_404(City, id=pk)
+        # context = {'object':city}
+        # return render(request, 'cities/detail.html', context)
+    if request.method == 'POST':
+        print(request.POST)
 
     qs = City.objects.all()
     context = {'objects_list': qs}
     return render(request, 'cities/home.html', context)
+
+class CityDetailView(DetailView):
+    queryset = City.objects.all()
+    template_name = 'cities/detail.html'
