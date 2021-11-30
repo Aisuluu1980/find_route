34983f6ecd5a0cd0dfec1a79869361bc37f5c598
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-11-30 07:31:28 +0600

    Added paginations.

--- cities/views.py
+++ cities/views.py
@@ -1,14 +1,17 @@
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, CreateView, UpdateView, DeleteView, ListView
 
 from cities.forms import HtmlForm, CityForm
 from cities.models import City
 
 __all__ = (
-    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView', 'CityDeleteView',
+    'home', 'CityDetailView', 'CityCreateView', 'CityUpdateView', 'CityDeleteView', 'CityListView',
 )
 
 def home(request, pk=None):
@@ -23,27 +26,34 @@ def home(request, pk=None):
         if form.is_valid():
             print(form.cleaned_data)
             form.save()
-    form = CityForm()
+    else:
+        form = CityForm()
     qs = City.objects.all()
-    context = {'objects_list': qs, 'form':form}
+    lst = Paginator(qs, 2)
+    page_number = request.GET.get('page')
+    page_obj = lst.get_page(page_number)
+
+    context = {'page_obj': page_obj, 'form': form}
     return render(request, 'cities/home.html', context)
 
 class CityDetailView(DetailView):
     queryset = City.objects.all()
     template_name = 'cities/detail.html'
 
-class CityCreateView(CreateView):
+class CityCreateView(SuccessMessageMixin, CreateView):
     model = City
     form_class = CityForm
     template_name = 'cities/create.html'
     # если success_url не указываю обязаьельно в model py прописываем функцию absolute_url
     success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно создан'
 
-class CityUpdateView(UpdateView):
+class CityUpdateView(SuccessMessageMixin, UpdateView):
     model = City
     form_class = CityForm
     template_name = 'cities/update.html'
     success_url = reverse_lazy('cities:home')
+    success_message = 'Город успешно отредактирован'
 
 class CityDeleteView(DeleteView):
     model = City
@@ -52,4 +62,17 @@ class CityDeleteView(DeleteView):
 
     # если удаление без подтверждения, сразу удаляет, template  тогда не указываю
     def get(self, request, *args, **kwargs):
+        messages.success(request, 'Город успешно удален')
         return self.post(self, request, *args, **kwargs)
+
+class CityListView(ListView):
+    paginate_by = 2
+    model = City
+    template_name = 'cities/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        form = CityForm()
+        context['form'] = form
+        return context
+
